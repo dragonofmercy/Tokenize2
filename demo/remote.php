f652cb00a02746d5c521e405013e6239a1797612
zellerda <dev@zellerda.com>
2016-06-13 14:24:58 +0200

    Add search from start

--- demo/remote.php
+++ demo/remote.php
@@ -2,11 +2,12 @@
 header('content-type: text/json');
 
 $obj = json_decode(file_get_contents('names.json'), true);
+$search = preg_quote($_REQUEST['search']);
 $ret = array();
 
 foreach($obj as $item)
 {
-    if((stripos($item['text'], $_REQUEST['search']) !== false) || $_REQUEST['search'] == '')
+    if(preg_match('/^' . $search . '/i', $item['text']))
     {
         $ret[] = array('value' => $item['text'], 'text' => $item['text']);
     }
