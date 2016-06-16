69532c16925465d567e540a03681309d31bf9f8a
zellerda <dev@zellerda.com>
2016-06-16 15:40:46 +0200

    Update docs

--- assets/application.js
+++ assets/application.js
@@ -1,32 +1,37 @@
 $(function(){
     // Scrollspy
     var $window = $(window);
-    var $body   = $(document.body);
+    var $body = $(document.body);
 
     $body.scrollspy({
         target: '.bs-docs-sidebar'
     });
-    $window.on('load', function () {
+    $window.on('load', function(){
         $body.scrollspy('refresh')
     });
 
     // Sidenav affixing
-    setTimeout(function () {
+    setTimeout(function(){
         var $sideBar = $('.bs-docs-sidebar');
 
         $sideBar.affix({
             offset: {
-                top: function () {
-                    var offsetTop      = $sideBar.offset().top;
-                    var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10);
+                top: function(){
+                    var offsetTop = $sideBar.offset().top;
+                    var sideBarMargin = parseInt($sideBar.children(0).css('margin-top'), 10);
                     var navOuterHeight = $('.bs-docs-nav').height();
 
                     return (this.top = offsetTop - navOuterHeight - sideBarMargin);
                 },
-                bottom: function () {
+                bottom: function(){
                     return (this.bottom = $('.bs-docs-footer').outerHeight(true));
                 }
             }
         })
     }, 100);
+
+    $('.tokenize-sortable-demo').on('tokenize:tokens:reorder tokenize:tokens:add', function(){
+        $('#tokenize-sortable-demo-console').html($(this).data('tokenize2').toArray().join(', '));
+    });
+    $('.tokenize-sortable-demo').trigger('tokenize:tokens:reorder');
 });
