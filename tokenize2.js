4c1192e2e4d811ae94d03fd5590a7ac3c40d6541
dragonofmercy <dragonofmercy@gmail.com>
2021-06-02 11:25:10 +0200

    Fix custom token display bug

--- tokenize2.js
+++ tokenize2.js
@@ -1,5 +1,5 @@
 /*!
- * Tokenize2 v1.3.4 (https://github.com/dragonofmercy/Tokenize2)
+ * Tokenize2 v1.3.5 (https://github.com/dragonofmercy/Tokenize2)
  * Copyright 2016-2017 DragonOfMercy.
  * Licensed under the new BSD license
  */
@@ -67,7 +67,7 @@
         A: 65
     };
 
-    Tokenize2.VERSION = '1.3.4';
+    Tokenize2.VERSION = '1.3.5';
     Tokenize2.DEBOUNCE = null;
     Tokenize2.DEFAULTS = {
         tokensMaxItems: 0,
@@ -283,8 +283,9 @@
      */
     Tokenize2.prototype.tokenAdd = function(value, text, force){
 
+        console.log(value, text);
         value = this.escape(value);
-        text = this.escape(text) || value;
+        text = text === undefined ? value : this.escape(text);
         force = force || false;
         this.resetInput();
 
